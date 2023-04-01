abe94eb76c634f9edf121200f405889b8122c2d4
Mateusz Cholewa <m.cholewa@vp.pl>
2023-04-01 22:19:35 +0200

    filtering in backend added

--- backend/database/db.js
+++ backend/database/db.js
@@ -21,7 +21,24 @@ var DataBase = {
                 console.log("Inserted one row");
             }
         });
+    },
+    getFilteredRooms: function (name = "", number_of_seats = 0, date = "", callback) {
+        let number_of_seats_step = number_of_seats === 0 ? 140 : 10
+        const sql = `SELECT DISTINCT NazwaSali FROM sale NATURAL JOIN rezerwacje WHERE
+            sale.IloscMiejsc BETWEEN ${number_of_seats} AND ${number_of_seats + number_of_seats_step}
+            AND sale.NazwaSali LIKE '%${name}%'
+            AND rezerwacje.Data LIKE '%${date}%'`;
+        connection.query(sql, function(err, result) {
+            if (err) {
+                console.log("Can't filter data");
+            }
+            else {
+                console.log("Data filtered");
+                callback(err, result);
+            }
+        });
     }
+
 };
 
 module.exports = DataBase;
--- backend/server.js
+++ backend/server.js
@@ -25,3 +25,17 @@ app.route('/room-list').get(function(req, res) {
 app.route('/add-room').post(function(req, res) {
     DataBase.insertRoom(req.body.id, req.body.seats, req.body.name);
 });
+
+app.route('/filter-rooms').post((req, res) => {
+    const name = req.body.name;
+    const seats = req.body.seats;
+    const date = req.body.date;
+    DataBase.getFilteredRooms(name, seats, date, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.send(result);
+        }
+    });
+});

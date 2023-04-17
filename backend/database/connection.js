43854c1bdff11ef31af64dc57b69b75d6c453293
Kacper Kubit <kacper.kubit99@gmail.com>
2023-04-17 23:29:23 +0200

    .

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,9 +1,8 @@
 var mysql = require("mysql");
 var connection = mysql.createConnection({
     host     : 'localhost',
-    user     : 'booking_system',
-    password : '12345',
-    database : 'booking_system'
+    user     : 'root',
+    database : 'booking_system',
 });
 connection.connect();
 module.exports = connection;

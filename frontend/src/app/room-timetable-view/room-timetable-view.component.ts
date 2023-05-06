48bbe0dced4e812687289f3436e33f9c1ea14d30
Kacper Kubit <kacper.kubit99@gmail.com>
2023-05-06 16:13:35 +0200

    timetable changes

--- frontend/src/app/room-timetable-view/room-timetable-view.component.ts
+++ frontend/src/app/room-timetable-view/room-timetable-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 interface TimeSlot {
   hour: string;
@@ -7,6 +7,7 @@ interface TimeSlot {
     name: string;
     instructor: string;
     room: string;
+    duration: number;
   }[];
 }
 
@@ -20,59 +21,107 @@ interface WeeklySchedule {
   templateUrl: './room-timetable-view.component.html',
   styleUrls: ['./room-timetable-view.component.css'],
 })
-export class RoomTimetableViewComponent {
+export class RoomTimetableViewComponent implements OnInit {
   days = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek'];
   yearlySchedule: WeeklySchedule[] = [
     {
       weekNumber: 1,
       slots: [
         {
-          hour: '8:00 - 10:00',
+          hour: '8:00',
           lessons: [
             {
               day: 'Poniedziałek',
               name: 'E-bizes',
               instructor: 'Jan Kowalski',
               room: 'Sala A-2-2',
+              duration: 2,
             },
-            // Dodaj pozostałe zajęcia dla tego przedziału godzinowego
           ],
         },
         {
-          hour: '10:00 - 12:00',
-          lessons: [
-            // Dodaj zajęcia dla tego przedziału godzinowego
-          ],
+          hour: '9:00',
+          lessons: [],
         },
         {
-          hour: '12:00 - 14:00',
+          hour: '10:00',
           lessons: [
             {
-              day: 'Środa',
+              day: 'Wtorek',
               name: 'Analiza',
               instructor: 'Jan Kowalski',
               room: 'Sala A-2-1',
+              duration: 1,
             },
-            // Dodaj zajęcia dla tego przedziału godzinowego
           ],
         },
         {
-          hour: '14:00 - 16:00',
+          hour: '11:00',
           lessons: [
-            // Dodaj zajęcia dla tego przedziału godzinowego
+            {
+              day: 'Środa',
+              name: 'Programowanie',
+              instructor: 'Jan Kowalski',
+              room: 'Sala A-2-3',
+              duration: 3,
+            },
           ],
         },
         {
-          hour: '16:00 - 18:00',
+          hour: '12:00',
+          lessons: [],
+        },
+        {
+          hour: '13:00',
+          lessons: [],
+        },
+        {
+          hour: '14:00',
           lessons: [
-            // Dodaj zajęcia dla tego przedziału godzinowego
+            {
+              day: 'Czwartek',
+              name: 'Matematyka',
+              instructor: 'Jan Kowalski',
+              room: 'Sala A-2-4',
+              duration: 2,
+            },
           ],
         },
         {
-          hour: '18:00 - 20:00',
+          hour: '15:00',
+          lessons: [],
+        },
+        {
+          hour: '16:00',
           lessons: [
-            // Dodaj zajęcia dla tego przedziału godzinowego
+            {
+              day: 'Piątek',
+              name: 'Fizyka',
+              instructor: 'Jan Kowalski',
+              room: 'Sala A-2-5',
+              duration: 1,
+            },
           ],
+        },
+        {
+          hour: '17:00',
+          lessons: [],
+        },
+        {
+          hour: '18:00',
+          lessons: [
+            {
+              day: 'Piątek',
+              name: 'Historia',
+              instructor: 'Jan Kowalski',
+              room: 'Sala A-2-6',
+              duration: 2,
+            },
+          ],
+        },
+        {
+          hour: '19:00',
+          lessons: [],
         },
       ],
     },
@@ -80,76 +129,117 @@ export class RoomTimetableViewComponent {
       weekNumber: 2,
       slots: [
         {
-          hour: '8:00 - 10:00',
+          hour: '8:00',
           lessons: [
             {
-              day: 'Wtorek',
+              day: 'Poniedziałek',
               name: 'E-bizes',
               instructor: 'Jan Kowalski',
-              room: 'Sala 101',
+              room: 'Sala A-2-2',
+              duration: 2,
             },
-            // Dodaj pozostałe zajęcia dla tego przedziału godzinowego
           ],
         },
         {
-          hour: '10:00 - 12:00',
-          lessons: [
-            // Dodaj zajęcia dla tego przedziału godzinowego
-          ],
+          hour: '9:00',
+          lessons: [],
         },
         {
-          hour: '12:00 - 14:00',
-          lessons: [
-            // Dodaj zajęcia dla tego przedziału godzinowego
-          ],
+          hour: '10:00',
+          lessons: [],
         },
         {
-          hour: '14:00 - 16:00',
-          lessons: [
-            // Dodaj zajęcia dla tego przedziału godzinowego
-          ],
+          hour: '11:00',
+          lessons: [],
         },
         {
-          hour: '16:00 - 18:00',
-          lessons: [
-            // Dodaj zajęcia dla tego przedziału godzinowego
-          ],
+          hour: '12:00',
+          lessons: [],
         },
         {
-          hour: '18:00 - 20:00',
-          lessons: [
-            // Dodaj zajęcia dla tego przedziału godzinowego
-          ],
+          hour: '13:00',
+          lessons: [],
+        },
+        {
+          hour: '14:00',
+          lessons: [],
+        },
+        {
+          hour: '15:00',
+          lessons: [],
+        },
+        {
+          hour: '16:00',
+          lessons: [],
+        },
+        {
+          hour: '17:00',
+          lessons: [],
+        },
+        {
+          hour: '18:00',
+          lessons: [],
+        },
+        {
+          hour: '19:00',
+          lessons: [],
         },
       ],
     },
   ];
-  currentWeek = 1;
 
-  get slots(): TimeSlot[] {
-    const schedule = this.yearlySchedule.find(
-      (s) => s.weekNumber === this.currentWeek
-    );
-    return schedule ? schedule.slots : [];
+  ngOnInit() {
+    this.updateSlots();
   }
 
+  currentWeek = 1;
+  slots: TimeSlot[] = [];
+
   getLesson(slot: TimeSlot, day: string) {
-    return slot.lessons.find((l) => l.day === day);
+    const lesson = slot.lessons.find((l) => l.day === day);
+    if (lesson) {
+      const startHour = parseInt(slot.hour.split(':')[0]);
+      const endHour = startHour + lesson.duration;
+      return {
+        ...lesson,
+        startTime: `${startHour}:00`,
+        endTime: `${endHour}:00`,
+      };
+    }
+    return null;
+  }
+
+  isInMergedSlot(slot: TimeSlot, day: string) {
+    const lesson = this.getLesson(slot, day);
+    return lesson && lesson.duration > 1;
   }
 
   printSchedule() {
     window.print();
   }
 
+  updateSlots() {
+    const schedule = this.yearlySchedule.find(
+      (s) => s.weekNumber === this.currentWeek
+    );
+    if (schedule) {
+      this.slots = schedule.slots;
+    } else {
+      this.slots = [];
+    }
+  }
+
   previousWeek() {
     if (this.currentWeek > 1) {
       this.currentWeek -= 1;
+      this.updateSlots();
     }
   }
 
   nextWeek() {
     if (this.currentWeek < this.yearlySchedule.length) {
       this.currentWeek += 1;
+      this.updateSlots();
     }
   }
 }

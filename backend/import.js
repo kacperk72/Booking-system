6adcec345793f45863d0abeb227311adbf4dd4a6
Łukasz Konieczny <ftefrjbhfvasf32@o2.pl>
2023-05-10 20:28:03 +0200

    Importing room info from USOS

--- backend/import.js
+++ backend/import.js
@@ -5,6 +5,7 @@ const https = require('https');
 const querystring = require('querystring');
 const express = require('express');
 const fs = require('fs');
+const roomIDs = require('./roomIDs.js');
 const app = express();
 
 const CONSUMER_KEY = 'xZNSd8Bww4vnmc3NwUwu';
@@ -12,8 +13,10 @@ const CONSUMER_SECRET = '65nWbEMCQDcpXyXYnsHR8RAQTKvYE7eRmaschvHu';
 const REQUEST_TOKEN_URL = 'https://apps.usos.uj.edu.pl/services/oauth/request_token';
 const ACCESS_TOKEN_URL = 'https://apps.usos.uj.edu.pl/services/oauth/access_token';
 const AUTHORIZE_URL = 'https://apps.usos.uj.edu.pl/services/oauth/authorize';
-let numbers = [3172, 3170, 3171, 3164, 3165, 3161, 3162, 3163, 3167, 3168, 3169, 3166, 3173, 3183, 3190, 3182, 3189,
-    3181, 3188, 3179, 3174, 3178, 3177, 3175, 3176, 3180, 3185, 3186, 3191, 3354];
+let numbers = [];
+
+// let numbers = [3172, 3170, 3171, 3164, 3165, 3161, 3162, 3163, 3167, 3168, 3169, 3166, 3173, 3183, 3190, 3182, 3189,
+//     3181, 3188, 3179, 3174, 3178, 3177, 3175, 3176, 3180, 3185, 3186, 3191, 3354];
 let allJsons = [];
 
 const oauth = new OAuth(
@@ -26,81 +29,83 @@ const oauth = new OAuth(
     'HMAC-SHA1'
 );
 
+(async () => {
+    numbers = await roomIDs.getRoomIDs();
+    // numbers.forEach(item => {
+    //     console.log(item);
+    // });
+    oauth.getOAuthRequestToken({oauth_callback: 'oob'}, function (err, oauthToken, oauthTokenSecret, results) {
+        if (err) {
+            console.log('Error getting OAuth request token:', err);
+        } else {
+            console.log('Authorize the app by visiting:', AUTHORIZE_URL + '?oauth_token=' + oauthToken);
 
-oauth.getOAuthRequestToken({oauth_callback: 'oob'}, function (err, oauthToken, oauthTokenSecret, results) {
-    if (err) {
-        console.log('Error getting OAuth request token:', err);
-    } else {
-        console.log('Authorize the app by visiting:', AUTHORIZE_URL + '?oauth_token=' + oauthToken);
+            const rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
 
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
+            rl.question('Enter the PIN code and hit ENTER when you\'re done: ', function (pin) {
+                rl.close();
+                oauth.getOAuthAccessToken(oauthToken, oauthTokenSecret, pin, function (err, oauthAccessToken, oauthAccessTokenSecret, results) {
+                    if (err) {
+                        console.log('Error getting OAuth access token:', err);
+                    } else {
 
-        rl.question('Enter the PIN code and hit ENTER when you\'re done: ', function (pin) {
-            rl.close();
-            oauth.getOAuthAccessToken(oauthToken, oauthTokenSecret, pin, function (err, oauthAccessToken, oauthAccessTokenSecret, results) {
-                if (err) {
-                    console.log('Error getting OAuth access token:', err);
-                } else {
+                        const url = 'https://apps.usos.uj.edu.pl/services/tt/room';
+                        const date = new Date();
+                        while (date <= new Date('2023-06-13')) {
 
-                    const url = 'https://apps.usos.uj.edu.pl/services/tt/room';
-                    const date = new Date();
-                    while (date <= new Date('2023-06-13')) {
-
-                        numbers.forEach((number) => {
-                            const roomId = number.toString();
-                            const oa = new OAuth(null, null, CONSUMER_KEY, CONSUMER_SECRET, '1.0', null, 'HMAC-SHA1');
-
-
-                            const formattedDate = date.toISOString().slice(0, 10);
-
-                            const queryParams = {
-                                room_id: roomId,
-                                start: formattedDate,
-                            };
-
-                            // const urlWithParams = `${url}?${querystring.stringify({room_id: roomId})}`;
-                            const urlWithParams = `${url}?${querystring.stringify(queryParams)}`
-                            oa.get(urlWithParams, oauthAccessToken, oauthAccessTokenSecret, function (err, data, response) {
-                                if (err) {
-                                    console.error(err);
-                                } else {
-                                    // console.log(response.statusCode);
-                                    let dataObject = JSON.parse(data);
+                            numbers.forEach((number) => {
+                                const roomId = number.toString();
+                                const oa = new OAuth(null, null, CONSUMER_KEY, CONSUMER_SECRET, '1.0', null, 'HMAC-SHA1');
 
 
-                                    let newData = dataObject.map(function (item) {
-                                        return {
-                                            id: number,
-                                            start_time: item.start_time,
-                                            end_time: item.end_time,
-                                            name_pl: item.name.pl
-                                        };
-                                    });
+                                const formattedDate = date.toISOString().slice(0, 10);
 
-                                    allJsons.push(newData)
+                                const queryParams = {
+                                    room_id: roomId,
+                                    start: formattedDate,
+                                };
+
+                                // const urlWithParams = `${url}?${querystring.stringify({room_id: roomId})}`;
+                                const urlWithParams = `${url}?${querystring.stringify(queryParams)}`
+                                oa.get(urlWithParams, oauthAccessToken, oauthAccessTokenSecret, function (err, data, response) {
+                                    if (err) {
+                                        console.error(err);
+                                    } else {
+                                        // console.log(response.statusCode);
+                                        let dataObject = JSON.parse(data);
 
 
-                                }
+                                        let newData = dataObject.map(function (item) {
+                                            return {
+                                                id: number,
+                                                start_time: item.start_time,
+                                                end_time: item.end_time,
+                                                name_pl: item.name.pl
+                                            };
+                                        });
+
+                                        allJsons.push(newData)
+
+
+                                    }
+                                });
+
                             });
 
-                        });
+                            date.setDate(date.getDate() + 7);
 
-                        date.setDate(date.getDate() + 7);
-
+                        }
                     }
-                }
+                });
             });
-        });
 
-    }
-    return allJsons;
-});
-
-
-
+        }
+        return allJsons;
+    });
+})();
 
 app.get('/data', (req, res) => {
     allJsons = JSON.stringify(allJsons)
@@ -114,6 +119,5 @@ app.get('/data', (req, res) => {
 
 app.listen(8001, () => {
 });
-
 
 
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 var DataBase = require('./database/db.js');
 var script = require('./import.js');
+const roomIDs = require('./roomIDs.js');
 var app = express();
 
 app.listen(3000);
@@ -89,29 +90,52 @@ app.route('/admin/reservation').put( (req,res) => {
 
 app.route('/addToDb').get((req, res) =>{
     var count = 0;
-    fetch('http://localhost:8001/data')
-    .then(response => response.json())
-    .then(data => {
-        // to wyswietla, ponizej pomiedzy komentarzami jak rozumie dodadnie do bazy
-        // console.log(JSON.stringify(data))
-        const parsedData = JSON.parse(data);
-        // console.log(parsedData);
-        // parsedData.forEach(firstTable => {
-        //     console.log(firstTable[0]);
-        //     DataBase.insertRoom(firstTable[0].id, 30, "Empty", "Empty");
+    let numbers = [];
+    (async () => {
+        numbers = await roomIDs.getRoomIDs();
+        // numbers.forEach(item => {
+        //     console.log(item);
         // });
-
-        parsedData.forEach(firstTable => {
-            firstTable.forEach(item => {
-                console.log(item.name_pl);
-                DataBase.insertRoom(item.id, 30, "Empty", "Empty");
-                DataBase.insertReservation(item.id, "Empty", item.name_pl, item.start_time, item.end_time, 'pending');
+        const addRoomsToDb = function(numbers, callback) {
+            numbers.forEach(async (item) => {
+                await fetch(`https://apps.usos.uj.edu.pl/services/geo/room?room_id=${item}&fields=number|type|capacity`)
+                .then(response => response.json())
+                .then(data => {
+                    const parsedData = JSON.parse(JSON.stringify(data));
+                    DataBase.insertRoom(item, parsedData.capacity, parsedData.number, parsedData.type);
+                })
+                .catch(err => {
+                    console.error(err);
+                });
             });
-        });
-    })
-    .catch(err => {
-        console.error(err);
-        res.status(500).send('Błąd pobrania danych');
-    });
+            callback();
+        };
+        const addReservationsToDb = function() {
+            fetch('http://localhost:8001/data')
+            .then(response => response.json())
+            .then(data => {
+                // to wyswietla, ponizej pomiedzy komentarzami jak rozumie dodadnie do bazy
+                // console.log(JSON.stringify(data))
+                const parsedData = JSON.parse(data);
+                // console.log(parsedData);
+                // parsedData.forEach(firstTable => {
+                //     console.log(firstTable[0]);
+                //     DataBase.insertRoom(firstTable[0].id, 30, "Empty", "Empty");
+                // });
+
+                parsedData.forEach(firstTable => {
+                    firstTable.forEach(item => {
+                        console.log(item.name_pl);
+                        DataBase.insertReservation(item.id, "Empty", item.name_pl, item.start_time, item.end_time, 'pending');
+                    });
+                });
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).send('Błąd pobrania danych');
+            });
+        };
+        addRoomsToDb(numbers, addReservationsToDb);
+    })();
 });
 
